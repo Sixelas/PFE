8f3cb9b3fbe3b30eaa7900ebbfde02885fea04d6
ahenquinet <alexis.henquinet@etu.u-bordeaux.fr>
2022-03-01 15:18:22 +0100

    serveurB VM

--- src/NEmu/network.py
+++ src/NEmu/network.py
@@ -16,17 +16,22 @@ ANDROID_PATH="/net/stockage/PFE-VPN-2022/android.img";
 
 ### Le mode cow pour les VFs va dériver l'image disque à partir de l'image de base
 VHost('serveur', conf='common', hds=[VFs(DEBIAN_PATH, 'cow', tag='serveur.img')], nics=[VNic(hw='0a:0a:0a:00:01:01'), VNic(hw='0a:0a:0a:00:01:02'), VNic(hw='0c:0c:0c:00:01:01')])
+VHost('serveurB', conf='common', hds=[VFs(DEBIAN_PATH, 'cow', tag='serveur.img')], nics=[VNic(hw='0a:0a:0a:00:03:01'), VNic(hw='0a:0a:0a:00:03:02'), VNic(hw='0c:0c:0c:00:01:02')])
 VHost('client', conf='common', hds=[VFs(ANDROID_PATH, 'cow', tag='client.img')], nics=[VNic(model='pcnet', hw='0a:0a:0a:00:02:01'), VNic(model='pcnet', hw='0a:0a:0a:00:02:02')])
 
 ### L'appel à SetIface n'est nécessaire que dans le cas où on a besoin de construire un réseau avec des VMs externes ou bien sur différentes machines physiques
 VSwitch('sw1', niface=3)
 Link(client='serveur:1', core='sw1:0')
 Link(client='client:1', core='sw1:1')
+Link(client='serveurB:1', core='sw1:2')
 
 VSlirp('slirp1', net='192.168.1.0/24')
 Link(client='serveur', core='slirp1')
 
 VSlirp('slirp2', net='192.168.2.0/24')
 Link(client='client:0', core='slirp2')
+
+VSlirp('slirp3', net='192.168.3.0/24')
+Link(client='serveurB', core='slirp3')
 
 StartNemu()
