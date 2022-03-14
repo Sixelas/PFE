6a4845b2236f9bc0b5178b3252c239c68263b8cf
Alexis-Portable <alexishenquinet@gmail.com>
2022-03-14 12:59:51 +0100

    clean code serverB UI

--- src/Cloud-Agent/Front-end/serveurB/serveurB_Front.py
+++ src/Cloud-Agent/Front-end/serveurB/serveurB_Front.py
@@ -12,17 +12,37 @@ from socket import *
 from netifaces import interfaces, ifaddresses, AF_INET
 
 
+
+# Dépendances manquantes sur les VM à installer :
+#
+# apt-get install python3-tk python3-pil python3-pil.imagetk
+# pip install pyqrcode netifaces pypng
+
+# ATTENTION Important :
+#
+# Script pensé pour être lancé sur ServeurB car il est root ! Sur pc perso faudra modif le code pour ajouter le "sudo" à von-network.
+# Pour l'instant ça lance les processus von-network et aca-py en arrière plan donc faut bien les kill avant de relancer quoi que ce soit avec :
+# 
+# Pour aca-py :
+# ps aux | grep aca-py
+# kill -9 <pid aca-py> 
+# 
+# Pour von-network :
+# ~/von-network/manage stop
+
 # /////// CONFIG ///////
 
 # Chemin du dossier qui contient ce fichier .py
 selfFolderPath = os.getcwd() 
 
 # Permet de récupérer automatiquement l'@ip de l'interface de la machine reliée au LAN. 
+# https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
 listeAdresses = []*len(interfaces())
 for ifaceName in interfaces():
     addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
     listeAdresses.append(addresses)
 
+# Ip de la machine reliée au von-network. Sur ServeurB c'est lui-même donc on laisse localhost
 genesisIP = 'localhost'
 
 VonStartCommand = "~/von-network/manage start logs"
@@ -36,15 +56,18 @@ RegisterCommand_3 = ''' curl -X POST "http://localhost:9000/register" -d '{"seed
 
 InvitCommand = ''' curl -X POST "http://localhost:11000/out-of-band/create-invitation" -H 'Content-Type: application/json' -d '{ "handshake_protocols": ["did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/didexchange/1.0"],"use_public_did": false}' > invitServeurtW.json '''
 
+# ///// END CONFIG /////
 
-#Commande pour lancer l'agent Von du ServeurB (en tâche de fond si possible, faut pas qu'il bloque le terminal). 
+
+
+# Commande pour lancer l'agent Von du ServeurB. 
 print("Démarrage du von-network ...")
 Vonproc = subprocess.Popen(VonStartCommand, shell=True, preexec_fn=os.setsid)
 Vonproc.wait()
-time.sleep(5)
+time.sleep(15)
 print("von-network OK http://localhost:9000/")
 
-#Commandes pour enregistrer les utilisateurs dans le von-network
+# Commandes pour enregistrer les utilisateurs dans le von-network
 print("Enregistrement des utilisateurs ...")
 subprocess.Popen(RegisterCommand_1, shell=True, preexec_fn=os.setsid)
 time.sleep(1)
@@ -54,19 +77,20 @@ subprocess.Popen(RegisterCommand_3, shell=True, preexec_fn=os.setsid)
 time.sleep(1)
 print("Utilisateurs enregistrés")
 
+
+# Commande pour lancer l'agent Cloud du ServeurB (en tâche de fond si possible, faut pas qu'il bloque le terminal). 
 print("Démarrage du CloudAgent ...")
-#Commande pour lancer l'agent Cloud du ServeurB (en tâche de fond si possible, faut pas qu'il bloque le terminal). 
 Agentproc = subprocess.Popen(AgentStartCommand, shell=True, preexec_fn=os.setsid)
 Agentproc.wait()
 time.sleep(5)
 print("CloudAgent ServeurB OK")
 
-print("Création de l'invitation pour ServeurW ...")
+
 #Commande pour lancer l'agent Cloud du ServeurB (en tâche de fond si possible, faut pas qu'il bloque le terminal). 
+print("Création de l'invitation pour ServeurW ...")
 Invitproc = subprocess.Popen(InvitCommand, shell=True, preexec_fn=os.setsid)
 Invitproc.wait()
 print("Invitation : ")
 subprocess.Popen("cat "+selfFolderPath+"/invitServeurtW.json", shell=True, preexec_fn=os.setsid)
 
 
-# ///// END CONFIG /////
